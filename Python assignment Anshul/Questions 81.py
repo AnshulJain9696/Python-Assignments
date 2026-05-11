0ff29f2d66092a0f03ea31492cd8da8e9557bb69
AnshulJain9696 <anahuljain9696@gmail.com>
2026-05-11 12:52:55 +0530

    Delete Python assignment Anshul/Questions 81.py

--- Python assignment Anshul/Questions 81.py
+++
@@ -1,5 +0,0 @@
-'''Write a Python program to write a list to a file. '''
-
-my_list = ["apple", "banana", "cherry", "date"]
-with open("d:\\tops\\python\\writ.txt", "a") as file:
-    print(file.writelines(my_list))
