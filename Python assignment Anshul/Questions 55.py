97d045043155f3ab160c5e5c1dfaf895c40dac02
AnshulJain9696 <anahuljain9696@gmail.com>
2026-05-15 13:02:01 +0530

    Update Questions 55.py

--- Python assignment Anshul/Questions 55.py
+++ Python assignment Anshul/Questions 55.py
@@ -1,5 +1,5 @@
 '''Write a Python script to merge two Python dictionaries'''
-dict1={"name":"sneha", "age":22,"course":"data analyst"}
-dict2={"city":"Dwarka","country":"India"}
+dict1={"name":"Anshul ", "age":22,"course":"data analyst"}
+dict2={"city":"Ajmer","country":"India"}
 dict1.update(dict2)
 print(dict1)
