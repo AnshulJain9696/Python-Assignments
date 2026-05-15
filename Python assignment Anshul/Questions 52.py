4ff835f5afda087b98970636d80a4388252917ab
AnshulJain9696 <anahuljain9696@gmail.com>
2026-05-15 12:57:41 +0530

    Update Questions 52.py

--- Python assignment Anshul/Questions 52.py
+++ Python assignment Anshul/Questions 52.py
@@ -1,6 +1,6 @@
 '''How Do You Check the Presence of a Key in A Dictionary?'''
 
-my_dict = {"name": "Sneha", "age": 22, "city": "Dwarka"}
+my_dict = {"name": "Anshul; jain", "age": 22, "city": "Bhilwara"}
 if  my_dict.get("age") :
     print("Key is present")
 else:
